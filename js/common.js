af16e19e465aea9a5dd85d320d52e30144465cd6
YuMikase <64767379+YuMikase@users.noreply.github.com>
2020-05-05 12:50:09 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 //目次のリストを設定
-var contents_list = ['Home', '楕円銀河について', '渦巻銀河について', '銀河の腕を描く（アニメーション）', '密度波（アニメーション）'];
+var contents_list = ['Home', '楕円銀河について', '渦巻銀河について', '密度波（アニメーション）'];
 //それぞれのhtmlファイル名
-var contents_list_file = ['index', 'ellipse', 'spiral', '', 'dwave'];
+var contents_list_file = ['index', 'ellipse', 'spiral', 'dwave'];
 //目次の要素数を取得
 var cont_len = contents_list.length;
 
