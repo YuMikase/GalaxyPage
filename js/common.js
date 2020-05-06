e74713414ab742ea0b4ecd1cafff834a879a77d6
YuMikase <64767379+YuMikase@users.noreply.github.com>
2020-05-06 20:03:32 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -21,6 +21,43 @@ function out_contents(n) {
     document.write("</div>");
 }
 
+/*----------題と目次の出力（アニメーション版）----------*/
+function out_contents_ani(n) {
+    /*アニメーションの秒数*/
+    var dura = 1000;
+    /*nav部分の高さ*/
+    var nav_height = 250;
+    /*要素の生成*/
+    document.write("<nav><p id='nav_title'>目次</p><ul>");
+    for (i = 0; i < contents_list.length; i++) {
+        if (i == n) {
+            var m = 'bor';
+        } else { var m = 'nbor' }
+        document.write("<li id='" + m + "'><a href='" + contents_list_file[i] + ".html'>" + contents_list[i]
+            + "</a></li> ");
+    }
+    /*アニメーションの設定*/
+    var navani = anime.timeline({
+        autoplay: false
+    });
+    navani
+        .add({
+            targets: "nav",
+            opacity: [0.0, 1.0],
+            height: [0, nav_height],
+            duration: dura*0.7,
+        })
+        .add({
+            targets: "nav > ul > li",
+            opacity: { value: [0.0, 1.0], duration: dura, delay: anime.stagger(100) },
+            translateY: { value: 30, duration: dura, delay: anime.stagger(100) },
+            rotateX: { value: 360, duration: dura, delay: anime.stagger(100) }
+        });
+    document.write("</ul></nav>");
+    document.querySelector('#navani_player').onclick = navani.play;
+}
+
+
 /*----------フッターの出力----------*/
 function out_footer() {
     document.write("<footer><small>&copy; 2020 Yu Mikase.</small ></footer >");
